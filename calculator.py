52252085f9b2ccd6d743e471007cb4a6135317a4
Tobias André <81941747+Tobias-Andre@users.noreply.github.com>
2022-04-25 12:36:05 +0200

    Functioning

--- calculator.py
+++ calculator.py
@@ -11,7 +11,8 @@ def create_window(theme):
             justification = 'right', 
             expand_x = True, 
             pad = (10,20),
-            right_click_menu = theme_menu)
+            right_click_menu = theme_menu,
+            key = '-TEXT-')
          ],
         [sg.Button('Clear', expand_x = True), sg.Button('Enter', expand_x = True)],
         [sg.Button(7, size = button_size), sg.Button(8, size = button_size), sg.Button(9, size = button_size), sg.Button('*', size = button_size)],
@@ -25,9 +26,38 @@ def create_window(theme):
 theme_menu = ['Menu', ['LightGrey1','dark','DarkGray8','random']]
 window = create_window('dark')
 
+current_num = []
+full_operation = []
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    
+    if event in theme_menu[1]:
+        window.close()
+        window = create_window(event)
         
+    if event in ['0','1','2','3','4','5','6','7','8','9','.']:
+        current_num.append(event)
+        num_string = ''.join(current_num)
+        window['-TEXT-'].update(num_string)
+
+    if event in ['*','/','+','-']:
+        full_operation.append(''.join(current_num))
+        current_num = []
+        full_operation.append(event)
+        window['-TEXT-'].update('')
+
+    if event == 'Enter':
+        full_operation.append(''.join(current_num))
+        result = eval(''.join(full_operation))
+        window['-TEXT-'].update(result)
+        full_operation = []
+
+    if event == 'Clear':
+        window['-TEXT-'].update('Output')
+        current_num = []
+        full_operation = []
+
 window.close()
